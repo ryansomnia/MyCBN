96e301058cefd026773cbb0ee3a1efbf41a9cb9f
ryansomnia <laskarimmanuel@gmail.com>
2022-03-09 11:55:06 +0700

    gox

--- App.js
+++ App.js
@@ -19,6 +19,9 @@ LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
 ]);
 LogBox.ignoreLogs(["EventEmitter.removeListener"]);
+LogBox.ignoreLogs([
+  "Warning: Failed prop type: Invalid prop `source` supplied to `Image`, expected one of type [number].",
+]);
 export default class App extends Component {
   render() {
     return (
